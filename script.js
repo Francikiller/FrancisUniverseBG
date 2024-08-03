a821d3981a9fad8e2c7f4da6957b7cc86f57cccb
Francikiller <71014301+Francikiller@users.noreply.github.com>
2024-08-04 01:09:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,11 +9,16 @@ document.addEventListener("DOMContentLoaded", function() {
         const left = Math.random() * 100 + 'vw';
         const top = Math.random() * 100 + 'vh';
         const duration = Math.random() * 5 + 5 + 's';
+        const blinkDuration = Math.random() * 2 + 0.5 + 's'; // Blinking duration
+        const blinkDelay = Math.random() * 2 + 's'; // Blinking delay
+
         circle.style.width = size;
         circle.style.height = size;
         circle.style.left = left;
         circle.style.top = top;
-        circle.style.animationDuration = duration;
+        circle.style.animationDuration = `${duration}, ${blinkDuration}`;
+        circle.style.animationDelay = `0s, ${blinkDelay}`; // Apply delay only to blinking
+
         container.appendChild(circle);
     }
 });
